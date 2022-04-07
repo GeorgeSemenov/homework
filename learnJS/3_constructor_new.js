d84fa0a1d72a9bf986bfe70666df57b88add0d7c
GeorgeSemenov <zhora.sem@yandex.ru>
2022-04-07 06:38:44 +0300

    ended up with constructors new

--- learnJS/3_constructor_new.js
+++ learnJS/3_constructor_new.js
@@ -1,24 +1,42 @@
 "use strict";
 console.log(`homework started`);
-
 const readline = require('readline-sync');
-function Calculator(){
-  this.firstOperand= 0;
-  this.secondOperand= 0;
 
-  this.read= function() {
-    this.firstOperand = readline.question(`Enter first operand: `);
-    this.secondOperand = readline.question(`Enter second operand: `);
-  };
-  this.sum = function(){
-    console.log(`sum = ${+this.firstOperand + +this.secondOperand}`);
-  };
-  this.mul = function(){
-    console.log(`multiplication = ${this.firstOperand * this.secondOperand}`);
-  };
+// function Calculator(){
+//   this.firstOperand= 0;
+//   this.secondOperand= 0;
+
+//   this.read= function() {
+//     this.firstOperand = readline.question(`Enter first operand: `);
+//     this.secondOperand = readline.question(`Enter second operand: `);
+//   };
+//   this.sum = function(){
+//     console.log(`sum = ${+this.firstOperand + +this.secondOperand}`);
+//   };
+//   this.mul = function(){
+//     console.log(`multiplication = ${this.firstOperand * this.secondOperand}`);
+//   };
+// }
+
+// let cal = new Calculator();
+// cal.read();
+// cal.sum();
+// cal.mul();
+
+//----
+
+function Accumulator(value) {
+  this.value=value;
+
+  this.read = function(){
+    let value = readline.question(`Enter value: `);
+    this.value+=+value;
+  }
 }
 
-let cal = new Calculator();
-cal.read();
-cal.sum();
-cal.mul();
+let accumulator = new Accumulator(1); // начальное значение 1
+
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению
+
+console.log(accumulator.value); // выведет сумму этих значений
