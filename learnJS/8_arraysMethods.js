5c8059764e47d7167ac2f574cb2babe512bda1cb
GeorgeSemenov <zhora.sem@yandex.ru>
2022-05-21 12:18:52 +0300

    methods of array

---
+++ learnJS/8_arraysMethods.js
@@ -0,0 +1,35 @@
+"use strict";
+console.log(`homework started`);
+const readline = require('readline-sync');
+// let value = readline.question(`Enter value: `);//пример запроса ввода пользователем
+
+
+// let str1 = `background-color`;
+// let str2 = `list-style-image`;
+// let str3 = `-webkit-transition`;
+
+// function camelize(str){
+//   return str.split('-').map((word,index)=>{
+//     return ( (index ==0) ? word: word[0].toUpperCase() + word.slice(1));
+//   }).join('');
+// }
+
+// console.log(`camelize(${str1}) = ${camelize(str1)}`);
+// console.log(`camelize(${str2}) = ${camelize(str2)}`);
+// console.log(`camelize(${str3}) = ${camelize(str3)}`);
+
+
+// function filterRange(arr, min, max){
+//   return arr.filter(item => (item>= min && item< max));
+// }
+// let arr = [5, 3, 8, 1];
+// let filtered = filterRange(arr,1,4)
+// console.log(`arr = ${arr}\nfiltered = ${filtered}`);
+
+
+function filterRangeInPlace(arr, min, max){
+  
+}
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr,1,4)
+console.log(`arr = ${arr}\nfiltered = ${filtered}`);
